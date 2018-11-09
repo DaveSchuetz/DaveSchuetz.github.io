d1e8d9418c4d0290e24ad0242be24a67cfa2f760
David Schuetz <38507773+DaveSchuetz@users.noreply.github.com>
2018-11-09 17:47:16 -0500

    Projects pages in

--- src/components/slides/Bookclub.js
+++ src/components/slides/Bookclub.js
@@ -3,12 +3,32 @@ import bc from "../../images/bookclub.png"
 import "./Jot.css"
 
 class Bookclub extends Component {
+  constructor(){
+    super()
+    this.state = {
+      visible: false,
+      more: true
+    }
+    this.showText = this.showText.bind(this)
+  }
+    showText (){
+      this.setState({
+        visible: !this.state.visible,
+        more: !this.state.more
+      })
+    }
     render() {
+      let visibility = this.state.visible ? "proj-visible" : "proj-hidden"
+      let more = this.state.more ? "more-btn" : "less-btn"
       return (
-        <div className="proj-vis">
+        <div>
             <img src={bc} alt="Jotpad" className="project-img" />
             <h4>Bookclub</h4>
-            <p className="proj-desc">Find and review books. Built with MongoDB, Express, React, and Node.</p>
+            <p className="proj-desc">Bookclub was a group project built with MongoDB, Express, React, and Node.</p><strong className={more} onClick={this.showText}>MORE</strong>
+            <div className={visibility}>
+              <p>Bookclub was built to be a place where you could come to find your next book to read. Someone could go to the page, search for a specific book, and leave comments. Or they could click the random button, and find the next book that they should read, either alone, or for their book-club.</p>
+              <p>This project tested our abilities to divide work and to work through GitHub conflicts. We did run into a few issues with merging into GitHub, but otherwise, our project went pretty smoothly. That is, until it came time to deploy. One thing that we saw was an issue while deploying to Surge, where it was having issues redirecting. This turns out was due to an issue Surge has with React routing, the resolution was to add a 200.html page, that Surge would use as the go to if the routing was not perfect, from there it would use React routing as expected. -- View <a href="http://mern-bookclub.surge.sh/" className="sentence-link" target="_blank" rel="noopener noreferrer">here</a>. See the code for the <a href="https://github.com/DaveSchuetz/BookClub-FrontEnd" className="sentence-link" target="_blank" rel="noopener noreferrer">front-end</a>, and the <a href="https://github.com/DaveSchuetz/BookClub-BackEnd" className="sentence-link" target="_blank" rel="noopener noreferrer">back-end</a>.</p><strong onClick={this.showText}>LESS</strong>
+            </div>
         </div>
       );
     }
--- src/components/slides/Jot.js
+++ src/components/slides/Jot.js
@@ -3,12 +3,32 @@ import jot from "../../images/jotpad.png"
 import "./Jot.css"
 
 class Jot extends Component {
+  constructor(){
+    super()
+    this.state = {
+      visible: false,
+      more: true
+    }
+    this.showText = this.showText.bind(this)
+  }
+    showText (){
+      this.setState({
+        visible: !this.state.visible,
+        more: !this.state.more
+      })
+    }
     render() {
+      let visibility = this.state.visible ? "proj-visible" : "proj-hidden"
+      let more = this.state.more ? "more-btn" : "less-btn"
       return (
-        <div className="proj-vis">
+        <div>
             <img src={jot} alt="Jotpad" className="project-img" />
             <h4>Jotpad</h4>
-            <p className="proj-desc">Notepad application built using React and Google Firebase.</p>
+            <p className="proj-desc">Notepad application built using React and Google Firebase. Firebase was used as the database, deployment, and authentication.</p><strong className={more} onClick={this.showText}>MORE</strong>
+            <div className={visibility}>
+              <p>Jotpad is a notepad application. Anyone can sign-up and start keeping notes, and access them anywhere on the web. A user will be able to get an e-mail in case they forgot their password. The site is responsive to mobile web viewing.</p>
+              <p>When someone signs in they will only be able to see the notes that they have created. This was the most straining part of the project. To start, I had to set it up so that when a note was created, it associated the user ID with it as the creator. Next it involved passing the authentication props through to the notes, and when querying the database, only searching based on the ID. -- View <a href="https://jot-pad-notes.firebaseapp.com/" className="sentence-link" target="_blank" rel="noopener noreferrer">here</a>. See the code on <a href="https://github.com/DaveSchuetz/jotpad" className="sentence-link" target="_blank" rel="noopener noreferrer">Github</a>.</p><strong onClick={this.showText}>LESS</strong>
+            </div>
         </div>
       );
     }
