60531bc7d9aef26f33dfe05bf7566cd8ed20be25
David Schuetz <38507773+DaveSchuetz@users.noreply.github.com>
2018-11-12 17:46:10 -0500

    Updates

---
+++ precache-manifest.658e6fa80f840b8c590602ca6ee74f30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "df0b07889f25bff4b2f1de331db743a2",
+    "url": "/static/media/silhouette.df0b0788.png"
+  },
+  {
+    "revision": "819ff6ae9ea4f119c2eeb4f34b942eaa",
+    "url": "/static/media/profile.819ff6ae.jpg"
+  },
+  {
+    "revision": "e47d67e0502274070220a6384d7f5e7f",
+    "url": "/static/media/jotpad.e47d67e0.png"
+  },
+  {
+    "revision": "38a08086f364e0450aa6b95cdb5d31ab",
+    "url": "/static/media/bookclub.38a08086.png"
+  },
+  {
+    "revision": "fcf8e905cd3ba4c0a88152b54ac6cf7a",
+    "url": "/static/media/Hanoi.fcf8e905.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9f128351e46bf57bfb0",
+    "url": "/static/js/main.e9f12835.chunk.js"
+  },
+  {
+    "revision": "01f664df9e0e5de937d9",
+    "url": "/static/js/1.01f664df.chunk.js"
+  },
+  {
+    "revision": "e9f128351e46bf57bfb0",
+    "url": "/static/css/main.975edbd9.chunk.css"
+  },
+  {
+    "revision": "498500a0249742fea1c5d848a321b0b2",
+    "url": "/index.html"
+  }
+];
